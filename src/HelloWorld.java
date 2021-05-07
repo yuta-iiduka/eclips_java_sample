8d1877570f3a9ed14a2ce48a536384fd38d3e5da
ota124 <ota12041204@gmail.com>
2021-05-07 18:19:37 +0900

    fix

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
-package sample;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
 }
--- src/module-info.java
+++
@@ -1,3 +0,0 @@
-module JavaSample {
-	exports sample;
-}
