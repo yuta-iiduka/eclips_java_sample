e680685c64c398be93ae8b6cc83d06f21ab5d078
ota124 <ota12041204@gmail.com>
2021-05-07 17:30:41 +0900

    fix

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module JavaSample {
+	exports sample;
 }
--- src/sample/JavaSampleClass.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public class JavaSampleClass {
-	public static void main(String[] args) {
-		System.out.println("Hello Java");
-		System.out.println("Hello Yuta");
-	}
-}
