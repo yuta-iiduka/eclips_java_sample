a24f1b88d7effc5b8507b9f89578eb93e8fcef65
ota124 <ota12041204@gmail.com>
2021-05-11 22:15:45 +0900

    main_method

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+
+
+public class Practice {
+
+	public static void main(String[] args) {
+		printData("Hello Java");
+	}
+	
+	public static void printData(String str) {
+		System.out.println(str);
+	}
+
+}
